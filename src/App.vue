37a80f0921a8886dd07ede27099475c8f04ea6be
PhengMenghour <phengmenghour@gmail.com>
2024-11-26 23:39:48 +0700

    Finish TP3 Step 03 Updated the new UI (Added MenuComponent & ProductComponent) Can filter Category and Product by Group using the Menu

--- src/App.vue
+++ src/App.vue
@@ -1,4 +1,14 @@
 <template>
+  <div>
+    <MenuComponent
+      v-for="menu in menus.filter((menu) => menu.id !== 2)"
+      :key="menu.id"
+      :textTitle="menu.textTitle"
+      v-bind="menu"
+      @groupSelected="updateCategoryGroup"
+    />
+  </div>
+
   <div class="row-1">
     <CategoryComponent
       v-for="category in categories"
@@ -19,14 +29,40 @@
       :buttonColor="promotion.buttonColor"
     />
   </div>
+
+  <div>
+    <MenuComponent
+      v-if="menus.find((menu) => menu.id === 2)"
+      :key="menus.find((menu) => menu.id === 2).id"
+      :textTitle="menus.find((menu) => menu.id === 2).textTitle"
+      v-bind="menus.find((menu) => menu.id === 2)"
+      @groupSelected="updateProductGroup"
+    />
+  </div>
+  <div class="row-3">
+    <ProductComponent
+      v-for="product in productsByGroup"
+      :key="product.id"
+      :name="product.name"
+      :rating="product.rating"
+      :size="product.size"
+      :images="product.image"
+      :price="product.price"
+      :promotionAsPercentage="product.promotionAsPercentage"
+      :categoryId="product.categoryId"
+      :instock="product.instock"
+      :coundSold="product.countSold"
+      :group="product.group"
+    />
+  </div>
 </template>
 
 <script>
 import CategoryComponent from "./components/CategoryComponent.vue";
 import PromotionComponent from "./components/PromotionComponent.vue";
 import ButtonComponent from "./components/ButtonComponent.vue";
-
-// import axios from "axios";
+import MenuComponent from "./components/MenuComponent.vue";
+import ProductComponent from "./components/ProductComponent.vue";
 import { useProductStore } from "./stores/Product";
 import { mapState } from "pinia";
 
@@ -35,17 +71,37 @@ export default {
     CategoryComponent,
     PromotionComponent,
     ButtonComponent,
+    MenuComponent,
+    ProductComponent,
   },
-
-  
 
   data() {
     return {
-      currentGroupname: "Group A",
+      currentProductGroup: null, //For product filtering
+      currentCategoryGroup:null, //For Category filtering
+
+      menus: [
+        {
+          id: 1,
+          textTitle: "Featured Categories",
+        },
+        {
+          id: 2,
+          textTitle: "Popular Component",
+        },
+      ],
     };
   },
 
-  methods: {},
+  methods: {
+    updateCategoryGroup(group){
+      this.currentCategoryGroup = group || null;
+    },
+
+    updateProductGroup(group){
+      this.currentProductGroup = group || null;
+    }
+  },
 
   mounted() {
     const productStore = useProductStore();
@@ -58,29 +114,37 @@ export default {
   computed: {
     // Map state and getters from the Pinia store
     ...mapState(useProductStore, {
-
       // Map the getter getPopularPRoduct to popularProducts
       popularProducts: "getPopularProduct",
 
       // Dynamically call getCategoriesByGroup with currentGroupName
       categories(store) {
-        return store.getCategoriesByGroup(this.currentGroupname);
+        if (!this.currentCategoryGroup) {
+          // If no group is selected, return all categories
+          return store.categories;
+        }
+        return store.getCategoriesByGroup(this.currentCategoryGroup);
       },
 
       // Dynamically call getProductsByGroup with currentGroupName
       productsByGroup(store) {
-        return store.getProductsByGroup(this.currentGroupname);
+        // return store.getProductsByGroup(this.currentGroupname);
+        
+        if (!this.currentProductGroup) {
+          return store.products;
+        }
+        return store.getProductsByGroup(this.currentProductGroup);
       },
 
       // Dynamically call getProductsByCategory
-      getProductsByCategory(store){
+      getProductsByCategory(store) {
         return store.getProductsByCategory;
       },
 
       // Get all products directly from the state
-      allProducts(store) {
-        return store.products;
-      },
+      // products(store) {
+      //   return store.products;
+      // },
 
       // categories(store){
       //   return store.categories;
@@ -96,7 +160,8 @@ export default {
 
 <style>
 .row-1,
-.row-2 {
+.row-2,
+.row-3 {
   display: flex;
   flex-wrap: wrap;
   justify-content: center;
--- src/stores/Product.js
+++ src/stores/Product.js
@@ -16,27 +16,11 @@ export const useProductStore = defineStore("product", {
         state.categories.filter((category) => category.group === groupName);
     },
 
-    /*
-      getCategoriesByGroup: (state) => (groupName) => {
-        return state.categories.filter(
-          (category) => category.groupName === groupName
-        );
-      },
-    */
-
     // getProductsByGroup(groupName): List all products by group name
     getProductsByGroup: (state) => {
       return (groupName) =>
         state.products.filter((product) => product.group === groupName);
     },
-
-    /*
-      getProductsByGroup: (state) => (groupName) => {
-        return state.products.filter(
-          (product) => product.groupName === groupName
-        );
-      },
-    */
 
     // getProductsByCategory(categoryId): List all products by given categoryId
     getProductsByCategory: (state) => {
@@ -44,25 +28,12 @@ export const useProductStore = defineStore("product", {
         state.products.filter((product) => product.categoryId === categoryId);
     },
 
-    /*
-      getProductsByCategory: (state) => (categoryId) => {
-        return state.products.filter(
-          (product) => product.categoryId === categoryId
-        );
-      },
-    */
-
     // getPopularProducts(): Any product with countSold > 10 is considered popular
-  getPopularProducts: (state) => {
-    const popular = () => state.products.filter((product) => product.countSold > 10);
-    return popular;
-  }
-
-    /*
-      getPopularProduct: (state) => {
-        return state.products.filter((product) => product.countSold > 10);
-      },
-    */
+    getPopularProducts: (state) => {
+      const popular = () =>
+        state.products.filter((product) => product.countSold > 10);
+      return popular;
+    },
   },
 
   actions: {
